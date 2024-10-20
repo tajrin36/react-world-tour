f82770e8160839f4677b12fdaa6ef2b5c344c98f
husna_tajrin <husnajahantajrin@gmail.com>
2024-10-20 21:15:28 +0600

    visited flag and visited countries section added

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,21 +1,65 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Country from "../Country/Country";
+import './Countries.css'
 
 const Countries = () => {
-    const [countries,setCountries] = useState([])
-    useEffect(()=>{
+
+    const [countries, setCountries] = useState([])
+
+    const [visitedCountries, setVisitedCountries] = useState([]);
+
+    const [visitedFlags, setVisitedFlags] = useState([]);
+
+
+    useEffect(() => {
         fetch("https://restcountries.com/v3.1/all")
-        .then(response => response.json())
-        .then(data => setCountries(data));
-    },[])
+            .then(response => response.json())
+            .then(data => setCountries(data));
+    }, []);
+
+    const handleVisitedCountries = country => {
+        console.log("Add this to your visited country")
+        const newVisitedCountries = [...visitedCountries, country];
+        setVisitedCountries(newVisitedCountries);
+    }
+
+    const handleVisitedFlags = flag => {
+        console.log("Add this to your visited flag");
+        const newVisitedFlags = [...visitedFlags, flag];
+        setVisitedFlags(newVisitedFlags);
+    }
     return (
+        // 
+
+
         <div>
-            <h4>countries: {countries.length}</h4>
-            {/* dynamic data load */}
-            {
-                countries.map(country => <Country key={country.cca3} country={country}></Country>)
-            }
+            <div className="flag-main-container">
+                <h2>Visited flags: {visitedFlags.length}</h2>
+                <div className="flag-container">
+                    {
+                        visitedFlags.map(flag => <img src={flag}></img>)
+                    }
+                </div>
+            </div>
+            <div className="list-container">
+                <h2>Visited countries: {visitedCountries.length}</h2>
+                <ul>
+                    {
+                        visitedCountries.map(country => <li className="list" key={country.cca3}>{country.name.common}</li>)
+                    }
+                </ul>
+            </div>
+            <div className="country-container">
+                {/* dynamic data load */}
+                {
+                    countries.map(country => <Country
+                        key={country.cca3}
+                        handleVisitedCountries={handleVisitedCountries}
+                        handleVisitedFlags={handleVisitedFlags}
+                        country={country}></Country>)
+                }
+            </div>
         </div>
     );
 };
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,11 +1,42 @@
+import { useState } from 'react';
 import './Country.css'
-const Country = ({country}) => {
+
+const Country = ({ country, handleVisitedCountries,handleVisitedFlags }) => {
     console.log(country);
-    const {name,flags} = country;
+    const [visited, setVisited] = useState(false);
+
+    const handleVisited = () => {
+        setVisited(!visited);
+    }
+
+    const { name, flags, area, capital, cca3, population, region, startOfWeek, status, subregion, car } = country;
+
+    const passWithParams = () => {
+        handleVisitedCountries(country);
+    }
+    const passFlagWithParams = () => {
+        handleVisitedFlags(country.flags.png);
+    }
     return (
-        <div className='country'>
-            <h3>name: {name?.common}</h3>
-            <img src={flags.png} alt="" />
+        <div className={`country ${visited ? 'visited' : 'non-visited'}`}>
+            <h3 style={{ color: visited ? 'purple' : 'white' }}>name: {name?.common}</h3>
+            <img className='country-image' src={flags.png} alt="" />
+            <p>Area: {area}</p>
+            <p>Capital: {capital}</p>
+            <p>Code: {cca3}</p>
+            <p>Population: {population}</p>
+            <p>Region: {region}</p>
+            <p>Start of week: {startOfWeek}</p>
+            <p>Status: {status}</p>
+            <p>Sub-region: {subregion}</p>
+            <p>Car-side: {car.side}</p>
+            <hr />
+            <div className='visit-btn'>
+                <button onClick={passFlagWithParams}>Visited flag</button>
+                <button onClick={passWithParams}>Mark visited</button>
+                <button onClick={handleVisited}>{visited ? 'Visited' : 'Going'}</button>
+                {visited ? 'I have visited this country' : 'I want to visit this country'}
+            </div>
         </div>
     );
 };
